9cb6c7805691e495131484fb0a30324a17b4a17f
Tim Purdum <tim@cedarrivertech.com>
2020-11-07 21:28:56 -0600

    lots of updates

---
+++ GamePadKeyboard/DPadControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GamePadKeyboard
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DPadControl : ContentView
+    {
+        private Constraint _buttonsY;
+
+        public DPadControl()
+        {
+            InitializeComponent();
+        }
+
+
+        public Constraint ButtonsY
+        {
+            get => _buttonsY;
+            set => _buttonsY = value;
+        }
+    }
+}
